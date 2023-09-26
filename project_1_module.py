465cd51e00ca1482f00bf5def7593920eb1261f4
ianstender <87210870+ianstender@users.noreply.github.com>
2023-09-26 12:22:23 -0400

    Update project_1_module.py

--- project_1_module.py
+++ project_1_module.py
@@ -5,5 +5,10 @@ Created on Tue Sep 26 11:03:30 2023
 
 @author: ianstender
 """
+import numpy as np
 
-indrgjldrgnjldrg  fovdl, 
+data = np.load('ecg_e0103_half1.npz')
+print(data.files)
+
+# def load_data(input_file)
+#     np.load(input_file)
