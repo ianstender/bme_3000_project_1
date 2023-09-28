9df364671c81576529bca7e1c6f044773fc1edac
ianstender <87210870+ianstender@users.noreply.github.com>
2023-09-28 11:22:31 -0400

    Finished until step 4

--- project_1_module.py
+++ project_1_module.py
@@ -6,9 +6,39 @@ Created on Tue Sep 26 11:03:30 2023
 @author: ianstender
 """
 import numpy as np
+from matplotlib import pyplot as plt
 
-data = np.load('ecg_e0103_half1.npz')
-print(data.files)
 
-# def load_data(input_file)
-#     np.load(input_file)
+
+
+def load_data(input_file):
+    data = np.load(input_file)
+    print(data.files)
+    ecg_voltage = data['ecg_voltage']
+    fs = data['fs']
+    label_samples = data['label_samples']
+    label_symbols = data['label_symbols']
+    subject_id = data['subject_id']
+    electrode = data['electrode']
+    units = data['units']
+    ecg_time = np.arange(0,len(ecg_voltage)*(1/fs),(1/fs))
+
+    return ecg_voltage,fs,label_samples,label_symbols,subject_id,electrode,units, ecg_time
+    
+    
+    
+def plot_raw_data(signal_voltage, signal_time, units = 'V', title = ''):
+    plt.figure(1,clear=True)
+    plt.plot(signal_time,signal_voltage)
+    plt.xlabel('Time (s)')
+    plt.ylabel(units)
+    plt.title(title)
+    plt.grid()
+    
+def plot_events(label_samples, label_symbols, signal_time, signal_voltage):
+    symbol_list = np.unique(label_symbols)
+    print(symbol_list)
+    for event_type in symbol_list:
+        event_sample = label_samples[label_symbols == event_type]
+        plt.scatter(signal_time[event_sample],signal_voltage[event_sample])
+    
--- project_1_script.py
+++ project_1_script.py
@@ -5,4 +5,14 @@ Created on Tue Sep 26 11:07:32 2023
 
 @author: ianstender
 """
+import numpy as np
+from matplotlib import pyplot as plt
 
+import project_1_module as module
+
+file_name = 'ecg_e0103_half1.npz'
+ecg_voltage,fs,label_samples,label_symbols,subject_id,electrode,units, ecg_time = module.load_data(file_name)
+
+module.plot_raw_data(ecg_voltage, ecg_time, 'Volts', 'ECG Data')
+
+module.plot_events(label_samples, label_symbols, ecg_time, ecg_voltage)
