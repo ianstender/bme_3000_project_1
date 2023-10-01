dd8d733ceba990d5e886fa352121a18415da1ceb
ianstender <87210870+ianstender@users.noreply.github.com>
2023-10-01 13:40:09 -0400

    finished up to part 5
    need to create a wrap funciton and calcualte standard dev and means

--- project_1_module.py
+++ project_1_module.py
@@ -10,6 +10,8 @@ from matplotlib import pyplot as plt
 
 
 
+# %% Part 1: Load Data
+
 
 def load_data(input_file):
     data = np.load(input_file)
@@ -24,21 +26,50 @@ def load_data(input_file):
     ecg_time = np.arange(0,len(ecg_voltage)*(1/fs),(1/fs))
 
     return ecg_voltage,fs,label_samples,label_symbols,subject_id,electrode,units, ecg_time
-    
-    
+
+# %% Part 2: Plot Raw Data
     
 def plot_raw_data(signal_voltage, signal_time, units = 'V', title = ''):
-    plt.figure(1,clear=True)
-    plt.plot(signal_time,signal_voltage)
+    plt.plot(signal_time,signal_voltage, label='signal')
     plt.xlabel('Time (s)')
     plt.ylabel(units)
     plt.title(title)
     plt.grid()
+    plt.legend()
     
+
+# %% Part 3: Plot Events
 def plot_events(label_samples, label_symbols, signal_time, signal_voltage):
-    symbol_list = np.unique(label_symbols)
-    print(symbol_list)
+    symbol_list = list(np.unique(label_symbols))
     for event_type in symbol_list:
         event_sample = label_samples[label_symbols == event_type]
-        plt.scatter(signal_time[event_sample],signal_voltage[event_sample])
+        plt.scatter(signal_time[event_sample],signal_voltage[event_sample], label=event_type)
+        plt.legend(loc='lower right')
+        
+
+# %% Part 4: Extract Trials
     
+def extract_trials(signal_voltage, trial_start_samples, trial_sample_count):
+    # Initialize an empty 2D array to store trials
+    trials = np.empty((len(trial_start_samples), trial_sample_count)) # creates empty array to hold trials
+
+    event_index = 0 # creates counter for row number in trial arrays
+    
+    # parse each row in trials, filling trial data in 
+    for row in trials:
+        start = trial_start_samples[event_index]
+        end = start + trial_sample_count
+        row_length = len(signal_voltage[start:end])
+        
+        # fills in complete rows 
+        if row_length == trial_sample_count:
+            trials[event_index,:] = signal_voltage[start:end]
+        
+        # fills in remaining values in row with nan values 
+        else: 
+            trials[event_index,:row_length] = signal_voltage[start:end]
+            trials[event_index,row_length:(trial_sample_count-row_length)] = np.nan
+        event_index +=1
+        
+    return trials
+
--- project_1_script.py
+++ project_1_script.py
@@ -10,9 +10,80 @@ from matplotlib import pyplot as plt
 
 import project_1_module as module
 
+# %% Load data
+
 file_name = 'ecg_e0103_half1.npz'
 ecg_voltage,fs,label_samples,label_symbols,subject_id,electrode,units, ecg_time = module.load_data(file_name)
 
-module.plot_raw_data(ecg_voltage, ecg_time, 'Volts', 'ECG Data')
 
+
+# %% Plot raw data
+plt.figure('Raw Data',clear=True)
+module.plot_raw_data(ecg_voltage, ecg_time, 'Volts', 'RAW ECG Data')
+
+
+# %% Plot events
 module.plot_events(label_samples, label_symbols, ecg_time, ecg_voltage)
+
+plt.xlim(1910,1912) # limit scope of plot to two normal beats and two arrhythmic beats
+
+
+# %% Extract Trials
+
+def check_size(trial_data, trial_name, expected_size):
+    if trial_data.shape == expected_size:
+        print(f'{trial_name} is correct size.')
+        
+    else: 
+        print(f'{trial_name} is not correct size.')
+    
+
+# determine start time and duration of trials
+t_start = -0.5
+duration = 1
+
+# create time array and figure for trials
+trial_time = np.arange(0,250*(1/fs),(1/fs))
+plt.figure('Trials', clear = True)
+
+
+
+# extract normal trials
+normal_samples = label_samples[label_symbols == 'N']
+
+normal_trial_start_samples = normal_samples + int(fs*t_start)
+
+normal_trial_sample_count = int(fs*duration)
+
+normal_trials = (module.extract_trials(ecg_voltage,normal_trial_start_samples,normal_trial_sample_count))
+
+
+# check if normal trials is correct size
+expected_trial_size = (len(normal_trial_start_samples),normal_trial_sample_count)
+
+check_size(normal_trials,'Normal Trials', expected_trial_size)
+
+
+# extract arrhythmic trials 
+arrhythmic_samples = label_samples[label_symbols == 'V']
+
+arrhythmic_trial_start_samples = arrhythmic_samples + int(fs*t_start)
+
+arrhythmic_trial_sample_count = int(fs*duration)
+
+arrhythmic_trials = (module.extract_trials(ecg_voltage,arrhythmic_trial_start_samples,arrhythmic_trial_sample_count))
+
+# check if arrhythmic trials is correct size
+trial_size = (len(normal_trial_start_samples),normal_trial_sample_count)
+
+check_size(normal_trials,'Arrythmic Trials', trial_size)
+
+
+# plot one normal trial and one arrhythmic trial
+plt.plot(trial_time,normal_trials[10], label = 'normal trial')
+plt.plot(trial_time,arrhythmic_trials[10], label = 'arrhythmic trial')
+plt.xlabel('Time (s)')
+plt.ylabel('V')
+plt.title('Normal trial vs. Arrhythmic Trial')
+plt.grid()
+plt.legend()
